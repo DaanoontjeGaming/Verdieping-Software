6844b218a26cf7b4534b8573cca5c3b5b2b42a48
Daan Maat <dmaat@roc-dev.com>
2023-09-07 12:52:24 +0200

    Finalized Upload

---
+++ WP-Plugin1.php
@@ -0,0 +1,9 @@
+<?php
+/** 
+* Plugin Name: Hello Web Project
+* Description: A standard plugin for printing "Hello there!" on your website.
+* Author: Daan Maat
+*/
+
+//Simple print for "Hello There"
+print "Hello there";
---
+++ WP-Plugin2.php
@@ -0,0 +1,9 @@
+<?php
+/** 
+* Plugin Name: Cute cat Project
+* Description: A standard plugin for showing a cute cat picture on your web page.
+* Author: Daan Maat
+*/
+
+//Simple code for showing the cat picture
+echo "<img src='https://dapzw.websmidconcept.nl/wp-content/uploads/2023/04/Geen-foto-gevonden-396x352.jpg'>";
